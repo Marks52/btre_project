52769755000895fcf5c979e765fde64592b22f2f
marks52 <marks52@hotmail.com>
2019-03-09 13:39:41 -0800

    removed email from settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -142,12 +142,7 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
-# Email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER='mdswanson52@gmail.com'
-EMAIL_HOST_PASSWORD='57Str%t52'
-EMAIL_USE_TLS=True
+
 
 try:
     from .local_settings import *
