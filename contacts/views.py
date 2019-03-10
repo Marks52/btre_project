c14f24b9da1ac22658073a5edc6e001eceece8fc
marks52 <marks52@hotmail.com>
2019-03-10 12:23:31 -0700

    disable email

--- contacts/views.py
+++ contacts/views.py
@@ -29,13 +29,13 @@ def contact(request):
         contact.save()
 
         # Send email
-        send_mail(
-           'Property Listing Inquiry',
-           'There has been an inquiry for ' + listing + '. Sign into the admin panel for more info',
-           'mdswanson52@gmail.com',
-           [realtor_email, 'marks52@hotmail.com'],
-           fail_silently=False
-        )
+        #send_mail(
+           #'Property Listing Inquiry',
+           #'There has been an inquiry for ' + listing + '. #Sign into the admin panel for more info',
+           #'mdswanson52@gmail.com',
+           #[realtor_email, 'marks52@hotmail.com'],
+           #fail_silently=False
+        #)
 
         messages.success(request, 'Your request has been submitted, a realtor will get back to you soon')
 
